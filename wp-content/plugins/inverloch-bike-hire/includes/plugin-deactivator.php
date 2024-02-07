0d9e68bdef3638d79f5d23f846300f7b160d7b9b
Joe Cin Ng <joe5763481@gmail.com>
2024-02-07 14:09:38 +1100

    Empty db related files

--- wp-content/plugins/inverloch-bike-hire/includes/plugin-deactivator.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function plugin_deactivator() {
-    // Include the file with database operations
-    include_once('db-operations.php');
-
-    // Call the function for table deletion
-    delete_plugin_tables();
-}
